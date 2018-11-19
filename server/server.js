5f959c173ca615eef99905c54f933d844db05c1e
Anders Ryden <rydena021@gmail.com>
2018-11-19 11:35:04 -0600

    adds env variable PORT

--- server/server.js
+++ server/server.js
@@ -2,16 +2,20 @@
 const express = require( 'express' );
 const app = express();
 const bodyParser = require( 'body-parser' );
+
 // uses
 app.use( express.static( './server/public' ) );
 app.use( bodyParser.urlencoded( { extended: true } ) );
+
 // globals
-const port = 5000;
+const port = process.env.PORT || 5000;
 let history = [];
+
 // spin up server
 app.listen( port, ()=>{
     console.log( 'server is up on:', port );
 }) // end server up
+
 // routes
 app.get( '/calculate', ( req, res )=>{
     console.log( '/calculate GET hit' );
